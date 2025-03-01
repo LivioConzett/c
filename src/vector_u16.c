3d6672cc3a875fc00659a4998961010ae5bef224
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2025-03-01 09:55:53 +0100

    changed the names of the function to reflect uint16

--- src/vector_u16.c
+++ src/vector_u16.c
@@ -11,7 +11,7 @@
  * \param two second number
  * \return gcd of the two numbers
  */
-uint16_t v_gcd(uint16_t one, uint16_t two){
+uint16_t v16_gcd(uint16_t one, uint16_t two){
 
     uint16_t result;
 
@@ -31,7 +31,7 @@ uint16_t v_gcd(uint16_t one, uint16_t two){
 /**
  * See header
  */
-void v_print(const vector_u16_t* vector, bool newline){
+void v16_print(const vector_u16_t* vector, bool newline){
     printf("%d : %d", vector->x, vector->y);
     if(newline) printf("\n");
 }
@@ -40,7 +40,7 @@ void v_print(const vector_u16_t* vector, bool newline){
 /**
  * See header
  */
-void v_add(vector_u16_t* one, vector_u16_t* two){
+void v16_add(vector_u16_t* one, vector_u16_t* two){
     one->x += two->x;
     one->y += two->y;
 }
@@ -48,7 +48,7 @@ void v_add(vector_u16_t* one, vector_u16_t* two){
 /**
  * See header
  */
-void v_subtract(vector_u16_t* one, vector_u16_t* two){
+void v16_subtract(vector_u16_t* one, vector_u16_t* two){
     one->x -= two->x;
     one->y -= two->y;
 }
@@ -56,7 +56,7 @@ void v_subtract(vector_u16_t* one, vector_u16_t* two){
 /**
  * See header
  */
-void v_multiply(vector_u16_t* vector, uint16_t multiplier){
+void v16_multiply(vector_u16_t* vector, uint16_t multiplier){
     vector->x *= multiplier;
     vector->y *= multiplier;
 }
@@ -64,7 +64,7 @@ void v_multiply(vector_u16_t* vector, uint16_t multiplier){
 /**
  * See header
  */
-void v_divide(vector_u16_t* vector, uint16_t divisor){
+void v16_divide(vector_u16_t* vector, uint16_t divisor){
     vector->x /= divisor;
     vector->y /= divisor;
 }
@@ -72,7 +72,7 @@ void v_divide(vector_u16_t* vector, uint16_t divisor){
 /**
  * See header
  */
-uint16_t v_get_higher(vector_u16_t* vector){
+uint16_t v16_get_higher(vector_u16_t* vector){
 
     if(vector->x > vector->y) return vector->x;
 
@@ -82,7 +82,7 @@ uint16_t v_get_higher(vector_u16_t* vector){
 /**
  * See header
  */
-uint16_t v_get_lower(vector_u16_t* vector){
+uint16_t v16_get_lower(vector_u16_t* vector){
 
     if(vector->x < vector->y) return vector->x;
 
@@ -92,7 +92,7 @@ uint16_t v_get_lower(vector_u16_t* vector){
 /**
  * See header
  */
-bool v_in_bound(vector_u16_t* vector, vector_u16_t* bounds){
+bool v16_in_bound(vector_u16_t* vector, vector_u16_t* bounds){
 
     if(vector->x < 0 || vector->x >= bounds->x) return 0;
     if(vector->y < 0 || vector->y >= bounds->y) return 0;
@@ -102,31 +102,31 @@ bool v_in_bound(vector_u16_t* vector, vector_u16_t* bounds){
 /**
  * See header
  */
-void v_flip(vector_u16_t* vector){
-    v_multiply(vector,-1);
+void v16_flip(vector_u16_t* vector){
+    v16_multiply(vector,-1);
 }
 
 /**
  * See header
  */
-bool v_equal(vector_u16_t* one, vector_u16_t* two){
+bool v16_equal(vector_u16_t* one, vector_u16_t* two){
     return one->x == two->x && one->y == two->y;
 }
 
 /**
  * See header
  */
-double v_get_magnitude(vector_u16_t* vector){
+double v16_get_magnitude(vector_u16_t* vector){
     return sqrt(pow(vector->x,2) + pow(vector->y, 2));
 }
 
 /**
  * See header
  */
-vector_u16_t v_get_unit_vector(vector_u16_t* vector){
+vector_u16_t v16_get_unit_vector(vector_u16_t* vector){
     vector_u16_t result;
 
-    uint32_t magnitude = v_get_magnitude(vector);
+    uint32_t magnitude = v16_get_magnitude(vector);
 
     result.x = vector->x / magnitude;
     result.y = vector->y / magnitude;
@@ -137,13 +137,13 @@ vector_u16_t v_get_unit_vector(vector_u16_t* vector){
 /**
  * See header
  */
-vector_u16_t v_get_direction(vector_u16_t* vector){
+vector_u16_t v16_get_direction(vector_u16_t* vector){
 
-    int denominator = v_gcd(vector->x, vector->y);
+    int denominator = v16_gcd(vector->x, vector->y);
 
     vector_u16_t result = *vector;
 
-    v_divide(&result, denominator);
+    v16_divide(&result, denominator);
 
     return result;
 }
--- src/vector_u16.h
+++ src/vector_u16.h
@@ -11,63 +11,70 @@ typedef struct {
 } vector_u16_t;
 
 
-static const vector_u16_t  N_V = {0,-1};
-static const vector_u16_t NE_V = {1,-1};
-static const vector_u16_t  E_V = {1,0};
-static const vector_u16_t SE_V = {1,1};
-static const vector_u16_t  S_V = {0,1};
-static const vector_u16_t SW_V = {-1,1};
-static const vector_u16_t  W_V = {-1,0};
-static const vector_u16_t NW_V = {-1,-1};
+const vector_u16_t  N_V16 = {0,-1};
+const vector_u16_t NE_V16 = {1,-1};
+const vector_u16_t  E_V16 = {1,0};
+const vector_u16_t SE_V16 = {1,1};
+const vector_u16_t  S_V16 = {0,1};
+const vector_u16_t SW_V16 = {-1,1};
+const vector_u16_t  W_V16 = {-1,0};
+const vector_u16_t NW_V16 = {-1,-1};
+
+const vector_u16_t NESW_V16[] = {
+    N_V16,
+    E_V16,
+    S_V16,
+    W_V16
+}; 
 
 /**
  * \brief print the vector
  * \param vector vector to print
  * \param newline print a new line or not
  */
-void v_print(const vector_u16_t* vector, bool newline);
+void v16_print(const vector_u16_t* vector, bool newline);
 
 /**
  * \brief add the second vector to the first
  * \param one first vector
  * \param two second vector
  */
-void v_add(vector_u16_t* one, vector_u16_t* two);
+void v16_add(vector_u16_t* one, vector_u16_t* two);
 
 /**
  * \brief subtract the second vector from the first
  * \param one first vector
  * \param two second vector
  */
-void v_subtract(vector_u16_t* one, vector_u16_t* two);
+void v16_subtract(vector_u16_t* one, vector_u16_t* two);
 
 /**
  * \brief multiply the vector by a number
  * \param vector vector to multiply
  * \param multiplier number to multiply by
  */
-void v_multiply(vector_u16_t* vector, uint16_t multiplier);
+void v16_multiply(vector_u16_t* vector, uint16_t multiplier);
 
 /**
  * \brief divide the vector by a number
  * \param vector vector to divide
  * \param divisor number to divide by
  */
-void v_divide(vector_u16_t* vector, uint16_t divisor);
+void v16_divide(vector_u16_t* vector, uint16_t divisor);
 
 /**
  * \brief get the higher of the two numbers in the vector
  * \param vector vector to get number from
  * \return the higher of the two numbers in the vector
  */
-uint16_t v_get_higher(vector_u16_t* vector);
+uint16_t v16_get_higher(vector_u16_t* vector);
 
 /**
  * \brief get the lower of the two numbers in the vector
  * \param vector vector to get number from
  * \return the lower of the two numbers in the vector
  */
-uint16_t v_get_lower(vector_u16_t* vector);
+uint16_t v16_get_lower(vector_u16_t* vector);
 
 /**
  * \brief check if the vector is within some bounds
@@ -75,13 +82,13 @@ uint16_t v_get_lower(vector_u16_t* vector);
  * \param bounds the bounds to check if the vector is in
  * \return 1 if vector is in bounds, else 0
  */
-bool v_in_bound(vector_u16_t* vector, vector_u16_t* bounds);
+bool v16_in_bound(vector_u16_t* vector, vector_u16_t* bounds);
 
 /**
  * \brief flip the vector
  * \param vector vector to flip
  */
-void v_flip(vector_u16_t* vector);
+void v16_flip(vector_u16_t* vector);
 
 /**
  * \brief check if two vectors are the same
@@ -89,28 +96,28 @@ void v_flip(vector_u16_t* vector);
  * \param two second vector
  * \return 1 if both vectors are the same, else 0
  */
-bool v_equal(vector_u16_t* one, vector_u16_t* two);
+bool v16_equal(vector_u16_t* one, vector_u16_t* two);
 
 /**
  * \brief get the magnitude of a vector
  * \param vector vector to get magnitude of
  * \return magnitude of vector
  */
-double v_get_magnitude(vector_u16_t* vector);
+double v16_get_magnitude(vector_u16_t* vector);
 
 /**
  * \brief get the unit vector of a vector
  * \param vector vector to get unit vector from
  * \return unit vector
  */
-vector_u16_t v_get_unit_vector(vector_u16_t* vector);
+vector_u16_t v16_get_unit_vector(vector_u16_t* vector);
 
 /**
  * \brief get the direction of the vector in a grid system
  * \param vector vector to get the direction from
  * \return vector of the direction
  */
-vector_u16_t v_get_direction(vector_u16_t* vector);
+vector_u16_t v16_get_direction(vector_u16_t* vector);
 
 
 #endif 
