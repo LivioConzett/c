9ee73fd6dda723aa28c766a0b35ce8d5016e9259
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2026-03-30 16:15:26 +0200

    added the threadpool init function

--- src/threadpool/README.md
+++ src/threadpool/README.md
@@ -1,1 +1,79 @@
 # threadpool
+
+A library using `pthreads`to create a threadpool.
+
+## Usage
+
+To use the library define `PQD_THREADPOOL_IMPLEMENTATION` and
+then include the `pqd_threadpool.h` file.  
+
+``` C
+#define PQD_THREADPOOL_IMPLEMENTATION
+#include "pqd_threadpool.h"
+
+int main(){
+
+    return 0;
+}
+```
+
+### declarations
+
+``` C
+typedef struct {
+  void (*function)(void* arg);
+  void* arg;
+} tp_task_t;
+
+typedef struct {
+  pthread_mutex_t lock;
+  pthread_cond_t notify;
+  pthread_t *threads;
+  tp_task_t *task_queue;
+  uint16_t thread_amount;
+  uint16_t task_amount;
+  uint16_t queued;
+  uint16_t queue_front;
+  uint16_t queue_back;
+  uint8_t stop;
+} tp_pool_t;
+
+int8_t tp_init(uint16_t thread_amount, uint16_t task_amount, tp_pool_t *threadpool);
+
+```
+
+### tp_init
+
+``` C
+int8_t tp_init(
+                uint16_t thread_amount,
+                uint16_t task_amount,
+                tp_pool_t *threadpool
+              );
+```
+
+#### Brief
+
+Initializes the threadpool. Mallocs the memory for the `task_queue` and the `threads`
+in the `tp_pool_t` structure.
+
+#### Params
+
+| param         | meaning                             |
+|---------------|-------------------------------------|
+| thread_amount | amount of threads to create in pool |
+| task_amount   | length of task array to allocate    |
+| threadpool    | thread pool to initialize           |
+
+#### Return
+
+| number | meaning                                                      |
+|--------|--------------------------------------------------------------|
+| 0      | success                                                      |
+| 1      | param `thread_amount` or `task_amount` is 0                  |
+| 2      | param `threadpool` is NULL                                   |
+| 3      | `pthread_mutex_init()` failed while initializing the `lock`  |
+| 4      | `pthread_cond_init()` failed while initializing the ``notify`|
+| 5      | `threads` malloc failed                                      |
+| 6      | `task_queue` malloc failed                                   |
+| 7      | `pthread_create()` failed while creating a thread            |
--- src/threadpool/pqd_threadpool.h
+++ src/threadpool/pqd_threadpool.h
@@ -1,34 +1,190 @@
-#ifndef THREADPOOL_H
-#define THREADPOOL_H
+/*
+* A thread pool useing pthreads.
+* Heavily inspired by Lowlevel (https://www.youtube.com/@LowLevelTV)
+*
+*
+*/
+
+#ifndef PQD_THREADPOOL_H
+#define PQD_THREADPOOL_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <pthread.h>
 
-#define THREADS 16
-#define QUEUE_SIZE 100
+// -----------------------------------------------------------------------------------
+// Structure declarations
+// -----------------------------------------------------------------------------------
 
+/**
+ * \brief structure for a task
+ * \param function function to run
+ * \param arg pointer to the args to pass to the function
+ */
 typedef struct {
-  void (*fn)(void* arg);
+  void (*function)(void* arg);
   void* arg;
-} task_t;
+} tp_task_t;
 
+/**
+ * \brief thread pool structure
+ * \param lock mutex to lock the threadpool struct
+ * \param notify condition to signal thread that new task has been added
+ * \param threads array of threads
+ * \param task_queue array of tasks to do
+ * \param thread_amount amount of threads in the pool
+ * \param task_amount length of task_queue array
+ * \param queued amount of task needed to be done in the task_queue
+ * \param queue_front first task in the queue
+ * \param queue_back last task in the queue
+ * \param stop stop bit to tell threads to end
+ */
 typedef struct {
   pthread_mutex_t lock;
   pthread_cond_t notify;
-  pthread_t threads[THREADS];
-  task_t task_queue[QUEUE_SIZE];
-  int queued;
-  int queue_front;
-  int queue_back;
-  int stop;
-} threadpool_t;
+  pthread_t *threads;
+  tp_task_t *task_queue;
+  uint16_t thread_amount;
+  uint16_t task_amount;
+  uint16_t queued;
+  uint16_t queue_front;
+  uint16_t queue_back;
+  uint8_t stop;
+} tp_pool_t;
 
+// -----------------------------------------------------------------------------------
 // Function declarations
-void threadpool_init(threadpool_t* pool);
-void threadpool_destroy(threadpool_t* pool);
-void threadpool_add_task(threadpool_t* pool, void (*function)(void*), void* arg);
+// -----------------------------------------------------------------------------------
+
+
+/**
+ * \brief Create and initialize the threadpool
+ * \param thread_amount amount of threads to create
+ * \param task_amount length of task queue to create
+ * \param pool pointer to threadpool to initialize
+ * \return 0 if success
+ */
+int8_t tp_init(uint16_t thread_amount, uint16_t task_amount, tp_pool_t *pool);
+
+/**
+ * \brief destroy a threadpool. Will stop all the threads and free all the memory
+ * \param pool threadpool to destroy
+ * \return 0 on success
+ */
+int8_t tp_destroy(tp_pool_t* pool);
+// void threadpool_add_task(threadpool_t* pool, void (*function)(void*), void* arg);
+
+
+
+// #ifdef PQD_THREADPOOL_IMPLEMENTATION
+// -----------------------------------------------------------------------------------
+// function definition
+// -----------------------------------------------------------------------------------
+
+
+/**
+ * See function declaration
+ */
+int8_t tp_init(uint16_t thread_amount, uint16_t task_amount, tp_pool_t *pool){
+    
+    // check the parameters
+    if(thread_amount <= 0 || task_amount <= 0) return 1;
+    if(pool == NULL) return 2;
+
+    pool->thread_amount = thread_amount;
+    pool->task_amount = task_amount;
+    pool->queued = 0;
+    pool->queue_front = 0;
+    pool->queue_back = 0;
+    pool->stop = 0;
+
+    // initialize the mutex
+    if(pthread_mutex_init(&(pool->lock), NULL) != 0) return 3;
+
+    // initialize the condition
+    if(pthread_cond_init(&(pool->notify), NULL) != 0){
+        pthread_mutex_destroy(&(pool->lock));
+        return 4;
+    }
+
+    // allocate the memory for the threads
+    pool->threads = (pthread_t *) malloc(thread_amount * sizeof(pthread_t));
+    if(pool->threads == NULL){
+        pthread_mutex_destroy(&(pool->lock));
+        pthread_cond_destroy(&(pool->notify));
+        return 5;
+    }
+
+    // allocate the memory for the task queue
+    pool->task_queue = (tp_task_t *) malloc(task_amount * sizeof(tp_task_t));
+    if(pool->task_queue = NULL){
+        free(pool->threads);
+        pool->threads = NULL;
+        pthread_mutex_destroy(&(pool->lock));
+        pthread_cond_destroy(&(pool->notify));
+        return 6;
+    }
+
+    // start the threads
+    for(uint16_t i = 0; i < thread_amount; i++){
+        if(pthread_create(&(pool->threads[i]), NULL, thread_function, pool) != 0){
+
+            // terminate all the threads that have been started if the creation 
+            // if them fails
+            pthread_mutex_lock(&(pool->lock));
+            pool->stop = 1;
+            pthread_cond_broadcast(&(pool->notify));
+            pthread_mutex_unlock(&(pool->lock));
+
+            for (uint16_t ii = 0; ii < i; ii++) {
+                pthread_join(pool->threads[ii], NULL);
+            }
+
+            pthread_mutex_destroy(&(pool->lock));
+            pthread_cond_destroy(&(pool->notify));
+
+            free(pool->task_queue);
+            pool->task_queue = NULL;
+            
+            free(pool->threads);
+            pool->threads = NULL;
+
+            return 7;
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * See function declaration
+ */
+int8_t tp_destroy(tp_pool_t* pool) {
+
+    pthread_mutex_lock(&(pool->lock));
+    pool->stop = 1;
+    pthread_cond_broadcast(&(pool->notify));
+    pthread_mutex_unlock(&(pool->lock));
+
+    for (uint16_t i = 0; i < pool->thread_amount; i++) {
+        pthread_join(pool->threads[i], NULL);
+    }
+
+    pthread_mutex_destroy(&(pool->lock));
+    pthread_cond_destroy(&(pool->notify));
+
+    free(pool->task_queue);
+    pool->task_queue = NULL;
+
+    free(pool->threads);
+    pool->threads = NULL;
+
+    pool = NULL;
+
+    return 0;
+}
 
 
 
@@ -61,34 +217,10 @@ void* thread_function(void* threadpool) {
 }
 
 
-void threadpool_init(threadpool_t *pool){
-    pool->queued = 0;
-    pool->queue_front = 0;
-    pool->queue_back = 0;
-    pool->stop = 0;
-
-    pthread_mutex_init(&(pool->lock), NULL);
-    pthread_cond_init(&(pool->notify), NULL);
-
-    for(int i = 0; i < THREADS; i++){
-        pthread_create(&(pool->threads[i]), NULL, thread_function, pool);
-    }
-}
 
 
-void threadpool_destroy(threadpool_t* pool) {
-    pthread_mutex_lock(&(pool->lock));
-    pool->stop = 1;
-    pthread_cond_broadcast(&(pool->notify));
-    pthread_mutex_unlock(&(pool->lock));
 
-    for (int i = 0; i < THREADS; i++) {
-        pthread_join(pool->threads[i], NULL);
-    }
 
-    pthread_mutex_destroy(&(pool->lock));
-    pthread_cond_destroy(&(pool->notify));
-}
 
 
 void threadpool_add_task(threadpool_t *pool, void (*function)(void*), void *arg){
@@ -107,5 +239,6 @@ void threadpool_add_task(threadpool_t *pool, void (*function)(void*), void *arg)
 
     pthread_mutex_unlock(&(pool->lock));
 }
+#endif // PQD_THREADPOOL_IMPLEMENTATION
 
-#endif // THREADPOOL_H 
+#endif // PQD_THREADPOOL_H 
